3c98012f58f51b7a8bef6139c21d6281a194fe0d
Anyqrh <49642406+Anyqrh@users.noreply.github.com>
2019-04-16 17:26:21 +0800

    Update test.java

--- src/KCG/GUI/test.java
+++ src/KCG/GUI/test.java
@@ -5,13 +5,11 @@ import java.awt.*;
 public class test{
     public static void main(String args[]){
         JFrame mainWindow = new JFrame("Knight Card Game");
-        MainPanel mainPanel = new MainPanel(Toolkit.getDefaultToolkit().getScreenSize());
+        MainPanel mainPanel = new MainPanel(Toolkit.getDefaultToolkit().getScreenSize());   
         mainWindow.add(mainPanel);
         mainWindow.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH);
         mainWindow.setVisible(true);
         mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        System.out.println("hellsssssssso");
-        System.out.println("·ëÌìÑô¼ÐÈû£¬¼Ð¿§°Ë");
     }
 }
